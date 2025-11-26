3f76dd6e4af8b8b8309559eae1595130bb4bfc6e
S.Teruyama <teruyama@springboard-inc.jp>
2025-11-27 01:51:41 +0900

    カードのタイトルを変更した時に反映されない不具合修正

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 import './style.scss';
 
-const main = () => {
+const main = (mutationList, observer) => {
     // カード列一覧
     const listElements = document.querySelectorAll('div[data-testid="list"]');
     for (const listElement of listElements) {
@@ -10,37 +10,43 @@ const main = () => {
         const elements = listElement.querySelectorAll('a[data-testid="card-name"]');
         for (const element of elements) {
             // タイトル処理
-            if (element.dataset.handlded === '1') {
-                // 既に処理済みならポイント和だけ更新
+            if (element.dataset.handled === '1') {
                 const estimatePoint = element.dataset.estimatePoint;
-                if (typeof estimatePoint !== 'undefined') {
+                const estimateElement = element.querySelector('span.th-point.estimate');
+                if (estimatePoint && estimateElement) {
+                    // 既に処理済みならポイント和だけ更新
                     sumEstimatePoint += parseFloat(estimatePoint);
-                    element.querySelector('span.th-point.estimate').title = sumEstimatePoint;
+                    estimateElement.title = sumEstimatePoint;
                 }
                 const actualPoint = element.dataset.actualPoint;
-                if (typeof actualPoint !== 'undefined') {
+                const actualElement = element.querySelector('span.th-point.actual');
+                if (actualPoint && actualElement) {
+                    // 既に処理済みならポイント和だけ更新
                     sumActualPoint += parseFloat(actualPoint);
-                    element.querySelector('span.th-point.actual').title = sumActualPoint;
+                    actualElement.title = sumActualPoint;
                 }
-            } else {
-                const originalText = element.textContent;
-                let highlightedText = '';
-                // () と [] で表された数値をポイントとしてハイライト
-                highlightedText = originalText
-                    .replace(/\(([0-9.?]+)\)/g, (_, numStr) => {
-                        element.dataset.estimatePoint = numStr;
-                        sumEstimatePoint += parseFloat(numStr);
-                        return `<span class="th-point estimate" title="${sumEstimatePoint}">${numStr}</span>`;
-                    })
-                    .replace(/\[([0-9.?]+)]/g, (_, numStr) => {
-                        element.dataset.actualPoint = numStr;
-                        sumActualPoint += parseFloat(numStr);
-                        return `<span class="th-point actual" title="${sumActualPoint}">${numStr}</span>`;
-                    });
-                element.innerHTML = highlightedText;
-                // 処理済みマーク付与
-                element.dataset.handlded = '1';
+                if (estimateElement || actualElement) {
+                    continue;
+                }
+                // handledでもspanが見つからなければ作り直し
             }
+            const originalText = element.textContent;
+            let highlightedText = '';
+            // () と [] で表された数値をポイントとしてハイライト
+            highlightedText = originalText
+                .replace(/\s*\(([0-9.?]+)\)\s*/g, (_, numStr) => {
+                    element.dataset.estimatePoint = numStr;
+                    sumEstimatePoint += parseFloat(numStr);
+                    return `<span class="th-point estimate" title="${sumEstimatePoint}">${numStr}</span>`;
+                })
+                .replace(/\s*\[([0-9.?]+)]\s*/g, (_, numStr) => {
+                    element.dataset.actualPoint = numStr;
+                    sumActualPoint += parseFloat(numStr);
+                    return `<span class="th-point actual" title="${sumActualPoint}">${numStr}</span>`;
+                });
+            element.innerHTML = highlightedText;
+            // 処理済みマーク付与
+            element.dataset.handled = '1';
         }
     }
 };
@@ -48,11 +54,13 @@ const main = () => {
 // DOM変更監視
 function debounce(fn, delay) {
     let timer;
-    return () => {
+    return (mutationList, observer) => {
         clearTimeout(timer);
-        timer = setTimeout(fn, delay);
+        timer = setTimeout(() => {
+            fn(mutationList, observer);
+        }, delay);
     };
 }
 
-const observer = new MutationObserver(debounce(main, 300));
+const observer = new MutationObserver(debounce(main, 2000));
 observer.observe(document.body, {childList: true, subtree: true});
